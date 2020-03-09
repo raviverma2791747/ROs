489fc1a054e7fdf858854c566f2ca572498fa85f
Ravi Verma <55842522+raviverma2791747@users.noreply.github.com>
2020-03-09 20:37:04 +0530

    Add files via upload

--- src/filesystem/fat.cpp
+++ src/filesystem/fat.cpp
@@ -1,1 +1,19 @@
 
+#include<filesystem/fat.h>
+
+using namespace myos;
+using namespace myos::common;
+using namespace myos::filesystem;
+using namespace myos::drivers;
+
+void ReadBiosBlock(myos::drivers::AdvanceTechnologyAttachement *hd,common::uint32_t partionOffset)
+{
+	BiosParameterBlock32 bpb;
+	hd->Read(partionOffset, (uint8_t*)&bpb,sizeof(BiosParameterBlock32));
+	for(int i= 0x00; i<sizeof(BiosParameterBlock32);i++)
+	{
+		printfHex(((uint8_t*)&bpb)[i]);
+		printf(" ");
+	}
+	printf("\n");
+}
